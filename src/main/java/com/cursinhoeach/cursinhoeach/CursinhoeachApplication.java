7a3c4b54493a78eba1f70bd1efa00c0c44ba3273
Andre Freire <and__re@hotmail.com>
2018-09-27 09:50:28 -0300

    first commit

---
+++ src/main/java/com/cursinhoeach/cursinhoeach/CursinhoeachApplication.java
@@ -0,0 +1,12 @@
+package com.cursinhoeach.cursinhoeach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursinhoeachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursinhoeachApplication.class, args);
+	}
+}
